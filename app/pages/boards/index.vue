a0c68df5d678a527a2fc31b7b9915ec644cb5088
Maxim Yunak <kleok0039@gmail.com>
2026-02-27 10:00:04 +0300

    feat(i18n): перевод страницы досок

--- app/pages/boards/index.vue
+++ app/pages/boards/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {useBoardStore} from "~/stores/boardStore";
+import { useBoardStore } from "~/stores/boardStore";
 
 const open = ref(false);
 
@@ -16,52 +16,79 @@ const create = () => {
   store.createBoard(newBoardData)
   open.value = false
 }
-
 </script>
 
 <template>
   <div>
-    <h2>Доски</h2>
+    <h2>{{ $t("boards.title") }}</h2>
+
     <div class="grid grid-cols-4 gap-4 mt-5">
-        <UPageCard
-            v-for="board in store.boards"
-            :to="`/boards/${board.id}`"
-            class="max-w-87.5"
-            variant="soft"
-        >
-          <template #default>
-            <span class="truncate max-w-80">{{board.name}}</span>
-          </template>
-        </UPageCard>
+      <UPageCard
+          v-for="board in store.boards"
+          :key="board.id"
+          :to="`/boards/${board.id}`"
+          class="max-w-87.5"
+          variant="soft"
+      >
+        <template #default>
+          <span class="truncate max-w-80">{{ board.name }}</span>
+        </template>
+      </UPageCard>
     </div>
 
-    <UModal v-model:open="open" title="Создать новую доску">
-      <UButton class="mt-10 flex justify-self-center">Создать новую доску</UButton>
+    <UModal
+        v-model:open="open"
+        :title="$t('boards.createModalTitle')"
+    >
+      <UButton
+          class="mt-10 flex justify-self-center"
+          @click="open = true"
+      >
+        {{ $t("boards.createButton") }}
+      </UButton>
+
       <template #body>
-
         <UForm>
-
-          <UFormField label="Название доски" required size="xl">
-            <UInput v-model="newBoardData.name" placeholder="Название доски"/>
+          <UFormField
+              :label="$t('boards.boardName')"
+              required
+              size="xl"
+          >
+            <UInput
+                v-model="newBoardData.name"
+                :placeholder="$t('boards.boardNamePlaceholder')"
+            />
           </UFormField>
 
-          <UFormField class="mt-2" label="Описание доски" size="xl">
-            <UInput v-model="newBoardData.description" placeholder="Описание доски"/>
+          <UFormField
+              class="mt-2"
+              :label="$t('boards.boardDescription')"
+              size="xl"
+          >
+            <UInput
+                v-model="newBoardData.description"
+                :placeholder="$t('boards.boardDescriptionPlaceholder')"
+            />
           </UFormField>
-
         </UForm>
-
-
       </template>
 
       <template #footer="{ close }">
-        <UButton label="Cancel" color="neutral" variant="outline" @click="close"/>
-        <UButton @click="create" label="Submit" color="neutral"/>
+        <UButton
+            :label="$t('boards.cancel')"
+            color="neutral"
+            variant="outline"
+            @click="close"
+        />
+        <UButton
+            @click="create"
+            :label="$t('boards.submit')"
+            color="neutral"
+        />
       </template>
     </UModal>
   </div>
 </template>
 
 <style scoped>
-
 </style>
