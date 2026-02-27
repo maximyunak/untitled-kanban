bcb0058496d75f3815da3f1a60fa1ac381ac0502
Maxim Yunak <kleok0039@gmail.com>
2026-02-27 13:14:20 +0300

    feat(i18n): перевод страницы invites

--- app/pages/invites.vue
+++ app/pages/invites.vue
@@ -8,27 +8,42 @@ const {
   declineInvite
 } = useInvite()
 
-await getMyInvites()
+await useAsyncData('invites', () => getMyInvites())
 </script>
 
 <template>
   <div>
-    <h2>My Invites</h2>
+    <h2>{{ $t('invites.title') }}</h2>
     <div class="flex flex-wrap max-w-[80vw] gap-4 mt-4">
-        <div v-if="invites.length !==0" v-for="invite in invites" class="w-87 rounded-lg bg-elevated/50 p-4 hover:bg-elevated transition max-w-87 flex justify-between">
+      <template v-if="invites.length !== 0">
+        <div
+          v-for="invite in invites"
+          class="w-87 rounded-lg bg-elevated/50 p-4 hover:bg-elevated transition max-w-87 flex justify-between"
+        >
           <div>{{ invite.board.name }}</div>
           <div class="flex gap-2" v-if="invite.status === InviteStatus.PENDING">
-
-            <UButton @click="acceptInvite(invite.id)" color="success" variant="subtle" icon="streamline:interface-validation-check-check-form-validation-checkmark-success-add-addition" />
-            <UButton @click="declineInvite(invite.id)" color="error" variant="subtle" icon="material-symbols:close"/>
+            <UButton
+              @click="acceptInvite(invite.id)"
+              color="success"
+              variant="subtle"
+              icon="streamline:interface-validation-check-check-form-validation-checkmark-success-add-addition"
+            />
+            <UButton
+              @click="declineInvite(invite.id)"
+              color="error"
+              variant="subtle"
+              icon="material-symbols:close"
+            />
           </div>
           <div v-else>
-            {{ invite.status.toLowerCase() }}
+            {{ $t(`invites.status.${invite.status.toLowerCase()}`) }}
           </div>
         </div>
-        <div v-else>
-          Инвайтов пока нет
-        </div>
+      </template>
+
+      <div v-else>
+        {{ $t('invites.empty') }}
+      </div>
     </div>
   </div>
 </template>
