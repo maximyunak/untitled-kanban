2dde75268cc77c37cbc324ea36ba72ea9f827d7b
Maxim Yunak <kleok0039@gmail.com>
2026-02-27 09:54:18 +0300

    fix: api файл исправил, убрал рефреш

--- app/plugins/api.ts
+++ app/plugins/api.ts
@@ -1,7 +1,7 @@
 export default defineNuxtPlugin(() => {
   const ssrHeaders = process.server ? useRequestHeaders(['cookie']) : undefined;
 
-  const api = $fetch.create({
+  const api: typeof $fetch = $fetch.create({
     baseURL: 'http://localhost:8000/api',
     credentials: 'include',
 
@@ -11,28 +11,6 @@ export default defineNuxtPlugin(() => {
           ...options.headers,
           ...ssrHeaders,
         };
-      }
-    },
-
-    async onResponseError({ response, options, request }) {
-      if (response.status === 401 && typeof request === 'string' && !request.includes('auth/login')) {
-        console.log('401 error', request);
-
-        try {
-          await $fetch('/auth/refresh', {
-            baseURL: 'http://localhost:8000/api',
-            credentials: 'include',
-            headers: ssrHeaders,
-          });
-
-
-          return $fetch(request, options);
-        } catch (e) {
-          console.error('Refresh failed', e);
-          if (process.client) {
-            navigateTo('/login');
-          }
-        }
       }
     },
   });
